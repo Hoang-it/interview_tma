944e8980404d741c030547313a914f70924c6781
hoang nguyen huu <hoang.nguyenhuu@lotussoft.vn>
2022-05-10 11:34:37 +0700

    Xong solution 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,49 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
+
+    public static int solution1(int numSteps){
+
+        return 0;
+    }
+
+    public static List<String> solution2(int numParenthesis){
+        List<String> cases = new ArrayList<>();
+        char[] str = new char[2 * numParenthesis];
+
+        if (numParenthesis > 0) calcParenthesisCase(cases, str,  0, numParenthesis, 0, 0);
+        return cases;
+    }
+
+    public static void calcParenthesisCase(List<String> cases, char str[], int pos, int n, int open, int close){
+        if(close == n)
+        {
+            // print the possible combinations
+            cases.add(new String(str));
+            return;
+        }
+        else
+        {
+            if(open > close) {
+                str[pos] = '}';
+                calcParenthesisCase(cases, str, pos+1, n, open, close+1);
+            }
+            if(open < n) {
+                str[pos] = '{';
+                calcParenthesisCase(cases, str, pos+1, n, open+1, close);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+	    int answer1 = solution1(3);
+        List<String> answer2 = solution2(3);
+
+        System.out.println("Answer for solution 1: " + answer1);
+        System.out.println("Answer for solution 2: " + answer2);
     }
 }
